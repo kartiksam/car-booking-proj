48cfc45e4222f2fa4fcc5ed3b52bba2c6c252ccf
kartik sharma <kartiksam123489@gmail.com>
2025-07-21 18:14:41 +0530

    twilio and half sockets integrated

--- booking-proj/src/otps/otps.module.ts
+++ booking-proj/src/otps/otps.module.ts
@@ -1,11 +1,9 @@
 import { forwardRef, Module } from '@nestjs/common';
-// import { OtpsController } from './otps.controller';
 import { OtpsService } from './otps.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Otp, OtpSchema } from './otp.schema';
 import { UsersModule } from 'src/users/users.module';
 import { EmailModule } from 'src/email/email.module';
-import { UserDetailsModule } from 'src/user-details/user-details.module';
 import { TwilioModule } from 'src/twilio/twilio.module';
 
 
